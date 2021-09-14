8d7c072516a31af3c4211e7f651fef1735c156c2
Michael Skov Sørensen <mich4e74@elev.tec.dk>
2021-09-14 13:03:57 +0200

    ProductRepo

---
+++ WebShop.API/Repository/ProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductById(int productId);
+        Task<Product> CreateProduct(Product product);
+        Task<Product> UpdateProduct(int productId, Product product);
+        Task<Product> DeleteProduct(int productId);
+    }
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository(WebShopContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _context.Product
+                .ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(int productId)
+        {
+            return await _context.Product
+                .FirstOrDefaultAsync(a => a.Id == productId);
+        }
+        public async Task<Product> CreateProduct(Product product)
+        {
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> DeleteProduct(int productId)
+        {
+            Product product = await _context.Product.FirstOrDefaultAsync(a => a.Id == productId);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            return product;
+        }
+
+
+        public async Task<Product> UpdateProduct(int productId, Product product)
+        {
+            Product updateProduct = await _context.Product.FirstOrDefaultAsync(a => a.Id == productId);
+            if (updateProduct !=null)
+            {
+                updateProduct.Name = product.Name;
+                updateProduct.Price = product.Price;
+                updateProduct.Description = product.Description;
+                await _context.SaveChangesAsync();
+            }
+            return updateProduct;
+        }
+    }
+}
