c641d2e55cf0642d591cb528dd6d9f18d3375981
christianmj <christianmj94@hotmail.dk>
2021-09-17 12:31:41 +0200

    mindre rettelser

--- WebShop.API/Services/ProductService.cs
+++ WebShop.API/Services/ProductService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebShop.API.Database.Entities;
@@ -138,5 +137,4 @@ namespace WebShop.API.Services
 
             }
         }
-    
 }
