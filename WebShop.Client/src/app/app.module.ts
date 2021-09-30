91dcc06e60a125890da38d4ccaa6a60e7837f541
Michael Skov Sørensen <mich4e74@elev.tec.dk>
2021-09-30 10:01:30 +0200

    admin component diff

--- WebShop.Client/src/app/app.module.ts
+++ WebShop.Client/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-// import { CategoryComponent } from './category/category.component';
-// import { ProductComponent } from './product/product.component';
-import { CategoryComponent } from './admin/category/category.component';
-import { ProductComponent } from './admin/product/product.component';
+import { CategoryComponent } from './category/category.component'; 
+import { ProductComponent } from './product/product.component';
+import { AdminCategoryComponent } from './admin/category/category.component';
+import { AdminProductComponent } from './admin/product/product.component';
 import { SearchComponent } from './search/search.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { CartComponent } from './cart/cart.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -28,6 +28,8 @@ import { FormsModule } from '@angular/forms';
     SearchComponent,
     ProductImageComponent,
     CartComponent,
+    AdminCategoryComponent,
+    AdminProductComponent
   ],
   imports: [
     BrowserModule,
