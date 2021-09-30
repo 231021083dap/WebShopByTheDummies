a3afaa0bb5f3f8430d2d5e5c56475f54aa1a6b1d
Michael Skov Sørensen <mich4e74@elev.tec.dk>
2021-09-30 09:45:30 +0200

    category service commit

--- WebShop.Client/src/app/category/category.component.ts
+++ WebShop.Client/src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../models';
+import { CategoryService } from '../_services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -10,9 +11,14 @@ export class CategoryComponent implements OnInit {
   categories: Category[] = [];
   
   
-  constructor() { }
+  constructor(
+    private categoryService:CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.categoryService.getCategories()    
+    .subscribe(c => this.categories = c);
+
     this.categories.push({ name: "Sport", picturePath: "assets/images/jakob-owens-j5kEQ1JLqZk-unsplash.jpg", id : 1} as Category);
     this.categories.push({ name: "Social", picturePath: "assets/images/michael-discenza-MxfcoxycH_Y-unsplash.jpg", id : 2} as Category);
     this.categories.push({ name: "Hangout", picturePath: "assets/images/nathan-dumlao-71u2fOofI-U-unsplash.jpg", id : 3} as Category);
